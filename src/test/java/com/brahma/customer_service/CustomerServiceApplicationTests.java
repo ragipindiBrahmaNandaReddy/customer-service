ee2b2557bb94ce290394e56f119d7f4f82ed290f
brahma <brahma45678@gmail.com>
2024-07-28 16:11:31 +0530

    customer-service skeleton

---
+++ src/test/java/com/brahma/customer_service/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brahma.customer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
